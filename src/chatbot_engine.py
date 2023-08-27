18a5784102d7a0a91ff15ab3ebe848f80ebe580a
kento <nerationj@gmail.com>
2023-08-27 17:41:18 +0900

    commit

--- src/chatbot_engine.py
+++ src/chatbot_engine.py
@@ -1,11 +1,11 @@
-from langchain.chat_models import ChatOpenAI
 import langchain
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ChatMessageHistory
 from langchain.schema import HumanMessage
 
 langchain.verbose = True
 
-def chat(message: str, history: ChatMessageHistory) ->str:
+def chat(message: str, history: ChatMessageHistory) -> str:
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
     messages = history.messages
--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,17 +1,19 @@
 import gradio as gr
 from dotenv import load_dotenv
 from chatbot_engine import chat
+
 from langchain.memory import ChatMessageHistory
 
-def respond(message, chat_history):
-        history = ChatMessageHistory
-        for [user_message, ai_message] in chat_history:
-             history.add_user_message(user_message)
-             history.add_ai_message(ai_message)
 
-        bot_message = chat(message, history)
-        chat_history.append((message, bot_message))
-        return "", chat_history
+def respond(message, chat_history):
+    history = ChatMessageHistory()
+    for [user_message, ai_message] in chat_history:
+            history.add_user_message(user_message)
+            history.add_ai_message(ai_message)
+
+    bot_message = chat(message, history)
+    chat_history.append((message, bot_message))
+    return "", chat_history
 
 with gr.Blocks() as demo:
     chatbot = gr.Chatbot()
